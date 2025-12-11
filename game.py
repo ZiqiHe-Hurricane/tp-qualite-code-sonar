e55aed91df73f6d1003d32010d4c82257291a7c5
ZiqiHe-Hurricane <ziqihe613@gmail.com>
2025-12-11 11:08:21 +0100

    game.py modified and test

--- game.py
+++ game.py
@@ -18,9 +18,24 @@ def move(player:str, position:tuple,board):
     print_board(board)
 
 def print_board(board):
-    for row in board:
-        print("|".join(row))
-        print("*" * 5)
+    display_board = []
+    for r in range(3):
+        row_display = []
+        for c in range(3):
+            if board[r][c] == " ":
+                row_display.append(f"{r},{c}")
+            else:
+                row_display.append(str(board[r][c]))
+        display_board.append(row_display)
+
+    cell_width = max(len(str(cell)) for row in board for cell in row)
+    if cell_width <1 :
+        cell_width = 1
+    for row_display in display_board:
+        formatted_cells = [str(cell).center(cell_width) for cell in row_display]
+        line = "|".join(formatted_cells)
+        print(line)
+        print("*" * len(line))
 
 def checkwinners(board):
     for row in range(3):
@@ -49,33 +64,44 @@ def main():
     player2 = input("Player2 :")
     while start :
         conf=input("Confirm game start ? Yes/No ")
-        if conf == "Yes" :
+        if conf in ("Yes", "yes", "YES") :
             print("The game starts !")
         else :
             print("Exit !")
             break
         board = board_init()
+        print_board(board=board)
         finish = False
         for i in range(9):
             if i%2 ==0:
-                step=input(f"Please {player1} make your move . (row col)")
-                row,col = map(int,step.split())
-                move(player=player1,position=(row,col),board=board)
-                if checkwinners(board=board):
-                    finish=True    
+                step=input(f"Please {player1} make your move. (row col)")
+                if step == "finish" :
+                    finish=False
                     break
+                else:
+                    clean = step.replace("(","").replace(")","").replace(","," ")
+                    row,col = map(int,clean.split())
+                    move(player=player1,position=(row,col),board=board)
+                    if checkwinners(board=board):
+                        finish=True    
+                        break
             
             else:
-                step=input(f"Please {player2} make your move . (row col)")
-                row,col = map(int,step.split())
-                move(player=player2,position=(row,col),board=board)
-                if checkwinners(board=board):
-                    finish=True
+                step=input(f"Please {player2} make your move. (row col)")
+                if step == "finish" :
+                    finish=False
                     break
+                else:
+                    clean = step.replace("(","").replace(")","").replace(","," ")
+                    row,col = map(int,clean.split())
+                    move(player=player2,position=(row,col),board=board)
+                    if checkwinners(board=board):
+                        finish=True
+                        break
         if finish == False:
             print("Game failed ! No winners !")
-        cont = input("Start next game ? Yes/No ")
-        if cont == "Yes":
+        conf = input("Start next game ? Yes/No ")
+        if conf in ("Yes", "yes", "YES"):
             start = True
         else : 
             start =False
